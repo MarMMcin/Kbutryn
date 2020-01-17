540849de277de24d875c7e5bcde9aedfea69e4b7
MarMMcin <em-erce@wp.pl>
2020-01-17 18:51:04 +0100

    Updates

---
+++ precache-manifest.9ac89d4da6114431d84fe7ccf35fd51e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cabab9d1d29e224e044b2422f6c31e",
+    "url": "/Kbutryn/index.html"
+  },
+  {
+    "revision": "b593e658136cbeffde37",
+    "url": "/Kbutryn/static/css/main.0e2013f3.chunk.css"
+  },
+  {
+    "revision": "9eaa8c5ceaaf78676fd2",
+    "url": "/Kbutryn/static/js/2.5cf4c833.chunk.js"
+  },
+  {
+    "revision": "b593e658136cbeffde37",
+    "url": "/Kbutryn/static/js/main.104d87e0.chunk.js"
+  },
+  {
+    "revision": "25f2d226f57619e41be3",
+    "url": "/Kbutryn/static/js/runtime~main.73831f4a.js"
+  },
+  {
+    "revision": "0b30d12b3f8f4d7677e87476557968b9",
+    "url": "/Kbutryn/static/media/Krk1.0b30d12b.jpg"
+  },
+  {
+    "revision": "d1cc6797fdf49ad39aa59e4d9bfdca7c",
+    "url": "/Kbutryn/static/media/Krk2.d1cc6797.jpg"
+  },
+  {
+    "revision": "a4ec57f965207bac64b110bf709376d7",
+    "url": "/Kbutryn/static/media/LogoIcon4.a4ec57f9.png"
+  },
+  {
+    "revision": "00b89701ff49a9a5c20ce866117dad52",
+    "url": "/Kbutryn/static/media/bg.00b89701.jpg"
+  },
+  {
+    "revision": "646f5605671171f3bcc56b4b80932f62",
+    "url": "/Kbutryn/static/media/geodezja.646f5605.jpg"
+  },
+  {
+    "revision": "16d6f93ba620ce0149ade2d7b0e24ca3",
+    "url": "/Kbutryn/static/media/geodezja2.16d6f93b.jpg"
+  },
+  {
+    "revision": "d8aa27abe6ab0855d768ea915b810252",
+    "url": "/Kbutryn/static/media/geodezja3.d8aa27ab.png"
+  },
+  {
+    "revision": "e99ff8db69f633c201eef329d1c86737",
+    "url": "/Kbutryn/static/media/inne.e99ff8db.jpg"
+  },
+  {
+    "revision": "af034ee65ecd2b38d26ce567068b70e2",
+    "url": "/Kbutryn/static/media/inne1.af034ee6.jpg"
+  },
+  {
+    "revision": "4c13e0ad53ba68fc44c313e200b8ba67",
+    "url": "/Kbutryn/static/media/kontakt1.4c13e0ad.jpg"
+  },
+  {
+    "revision": "b78f3dc5898a04bcc2e877b565a76262",
+    "url": "/Kbutryn/static/media/kontakt2.b78f3dc5.jpg"
+  },
+  {
+    "revision": "8c27832c915382d26b3bb9b532dd6422",
+    "url": "/Kbutryn/static/media/logo1.8c27832c.png"
+  },
+  {
+    "revision": "eb9cfbae2235a58ff7d24a748ff9f5a0",
+    "url": "/Kbutryn/static/media/logo2.eb9cfbae.png"
+  },
+  {
+    "revision": "1fea405c831fdcac09cb9a3bf424222e",
+    "url": "/Kbutryn/static/media/nieruch3.1fea405c.jpg"
+  },
+  {
+    "revision": "1f0b46e047d0793f9aefc90cc225a556",
+    "url": "/Kbutryn/static/media/nieruchomosc.1f0b46e0.jpg"
+  },
+  {
+    "revision": "3764e39b822138f7c31fe513866f21df",
+    "url": "/Kbutryn/static/media/nieruchomosc2.3764e39b.jpg"
+  },
+  {
+    "revision": "5d51ae97bc2b401d2644bb8e2dbcd989",
+    "url": "/Kbutryn/static/media/swiadectwo.5d51ae97.jpg"
+  }
+]);

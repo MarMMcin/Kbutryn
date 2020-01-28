f67d08c086e1e1c8f2ec397c6869c38f858a22d3
MarMMcin <em-erce@wp.pl>
2020-01-28 01:36:11 +0100

    Updates

---
+++ precache-manifest.81422c2af800d28a1f55f74d60681cfe.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b961f7103f78d3ede5e9cc5c4689aed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3598e250dd4db13d5db6",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "ffb0e7fb154cfd87af67",
+    "url": "/static/css/main.343a8092.chunk.css"
+  },
+  {
+    "revision": "3598e250dd4db13d5db6",
+    "url": "/static/js/2.9310eb0f.chunk.js"
+  },
+  {
+    "revision": "ffb0e7fb154cfd87af67",
+    "url": "/static/js/main.87df65c7.chunk.js"
+  },
+  {
+    "revision": "3abdfdc67514b98b5a8d",
+    "url": "/static/js/runtime~main.d2b0c258.js"
+  },
+  {
+    "revision": "0b30d12b3f8f4d7677e87476557968b9",
+    "url": "/static/media/Krk1.0b30d12b.jpg"
+  },
+  {
+    "revision": "d1cc6797fdf49ad39aa59e4d9bfdca7c",
+    "url": "/static/media/Krk2.d1cc6797.jpg"
+  },
+  {
+    "revision": "9971a88aa2d2896dcefb9eead1969ee0",
+    "url": "/static/media/LogoIcon4.9971a88a.png"
+  },
+  {
+    "revision": "aa3ad8a9daa8ec9d6018955708fe5737",
+    "url": "/static/media/PL.aa3ad8a9.svg"
+  },
+  {
+    "revision": "b67586f0eed39bd761c3f2f902cb7e99",
+    "url": "/static/media/artykul1.b67586f0.jpg"
+  },
+  {
+    "revision": "63ae2b44c7c31a027c401ae904849445",
+    "url": "/static/media/artykul10.63ae2b44.jpg"
+  },
+  {
+    "revision": "2f5222501baf9bd36172310509f314fc",
+    "url": "/static/media/artykul2.2f522250.jpg"
+  },
+  {
+    "revision": "1bd693b7c7a67c6aae0100134d7c4a82",
+    "url": "/static/media/artykul3.1bd693b7.jpg"
+  },
+  {
+    "revision": "74b1039b40dacb99df0c054d384be3c3",
+    "url": "/static/media/artykul4.74b1039b.jpg"
+  },
+  {
+    "revision": "9e740fb7b8fd9a371f86dd3da3eadcd3",
+    "url": "/static/media/artykul5.9e740fb7.jpg"
+  },
+  {
+    "revision": "eeef8fe90596ae764cf3d1164d7376b5",
+    "url": "/static/media/artykul6.eeef8fe9.jpg"
+  },
+  {
+    "revision": "15bccd88f8f48f03029092940742bbbe",
+    "url": "/static/media/artykul7.15bccd88.jpg"
+  },
+  {
+    "revision": "d6799f15d231fd3a69a50331af4de2a9",
+    "url": "/static/media/artykul8.d6799f15.jpg"
+  },
+  {
+    "revision": "1483573bc1fc0d77982c66beb6538487",
+    "url": "/static/media/artykul9.1483573b.jpg"
+  },
+  {
+    "revision": "00b89701ff49a9a5c20ce866117dad52",
+    "url": "/static/media/bg.00b89701.jpg"
+  },
+  {
+    "revision": "24e60e6f7d9b554c7d37dde1bb060dff",
+    "url": "/static/media/gbflag.24e60e6f.svg"
+  },
+  {
+    "revision": "646f5605671171f3bcc56b4b80932f62",
+    "url": "/static/media/geodezja.646f5605.jpg"
+  },
+  {
+    "revision": "16d6f93ba620ce0149ade2d7b0e24ca3",
+    "url": "/static/media/geodezja2.16d6f93b.jpg"
+  },
+  {
+    "revision": "d8aa27abe6ab0855d768ea915b810252",
+    "url": "/static/media/geodezja3.d8aa27ab.png"
+  },
+  {
+    "revision": "e99ff8db69f633c201eef329d1c86737",
+    "url": "/static/media/inne.e99ff8db.jpg"
+  },
+  {
+    "revision": "af034ee65ecd2b38d26ce567068b70e2",
+    "url": "/static/media/inne1.af034ee6.jpg"
+  },
+  {
+    "revision": "4c13e0ad53ba68fc44c313e200b8ba67",
+    "url": "/static/media/kontakt1.4c13e0ad.jpg"
+  },
+  {
+    "revision": "b78f3dc5898a04bcc2e877b565a76262",
+    "url": "/static/media/kontakt2.b78f3dc5.jpg"
+  },
+  {
+    "revision": "8c27832c915382d26b3bb9b532dd6422",
+    "url": "/static/media/logo1.8c27832c.png"
+  },
+  {
+    "revision": "eb9cfbae2235a58ff7d24a748ff9f5a0",
+    "url": "/static/media/logo2.eb9cfbae.png"
+  },
+  {
+    "revision": "2cd3b1b71d52129dd747dd43bd6a6efb",
+    "url": "/static/media/nieruch3.2cd3b1b7.jpg"
+  },
+  {
+    "revision": "1f0b46e047d0793f9aefc90cc225a556",
+    "url": "/static/media/nieruchomosc.1f0b46e0.jpg"
+  },
+  {
+    "revision": "3764e39b822138f7c31fe513866f21df",
+    "url": "/static/media/nieruchomosc2.3764e39b.jpg"
+  },
+  {
+    "revision": "d2931b66766f4ca66cdfac1f4a0a9bab",
+    "url": "/static/media/urban.d2931b66.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Kbutryn/precache-manifest.344b740f706916100d0ff0ee490f79d3.js"
+  "/precache-manifest.81422c2af800d28a1f55f74d60681cfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kbutryn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
